8a454b8aa3ed4daac358adc922da28e626c987ee
VictorMeyer77 <victormeyerc2i@gmail.com>
2024-07-04 16:01:55 +0200

    init with rg and vnet

--- databricks-azure/output.tf
+++ databricks-azure/output.tf
@@ -2,6 +2,6 @@ output "resource_group_id" {
   value = azurerm_resource_group.sbdrgdev.id
 }
 
-output "virtual_network_subnets" {
+output "virtual_network_subnet" {
   value = azurerm_virtual_network.sdbvnetdev.subnet
 }
